67346229f35fa49891f9c3522ced552c7e03383c
Sai Pranav <saipranav.sai2005@gmail.com>
2025-05-21 23:43:40 +0530

    test2

---
+++ src/main/java/com/appbuildersinc/attendance/source/restcontroller/Functions.java
@@ -0,0 +1,12 @@
+package com.appbuildersinc.attendance.source.restcontroller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Functions {
+    public ResponseEntity<String> helloworld() {
+        return ResponseEntity.ok("Hello, World!");
+    }
+}
