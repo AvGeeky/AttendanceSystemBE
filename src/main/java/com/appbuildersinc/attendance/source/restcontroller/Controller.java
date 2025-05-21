45876360880d1b2b939139e0ab800bea93c55434
AvGeeky <sai.saip2005@gmail.com>
2025-05-22 00:09:02 +0530

    test3

--- src/main/java/com/appbuildersinc/attendance/source/restcontroller/Controller.java
+++ src/main/java/com/appbuildersinc/attendance/source/restcontroller/Controller.java
@@ -8,19 +8,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
     private final Functions functionsService;
+    private final ResponseEntity<String> byeworld;
 
     @Autowired
-    public Controller(Functions functionsService) {
+    public Controller(Functions functionsService, ResponseEntity<String> byeworld) {
         this.functionsService = functionsService;
+        this.byeworld = byeworld;
     }
+
+
     /**
      * This endpoint returns a simple "Hello, World!" message.
      *
      * @return A ResponseEntity containing the "Hello, World!" message.
      */
     @GetMapping("/helloworld")
-    public ResponseEntity<String> setSecretId(){
+    public ResponseEntity<String> helloWorld(){
         return functionsService.helloworld();
     }
+
+    /**
+     * This endpoint returns a simple "Bye, World!" message.
+     *
+     * @return A ResponseEntity containing the "Bye, World!" message.
+     */
+    @GetMapping("/byeworld")
+    public ResponseEntity<String> byeWorld(){
+        return byeworld;
+    }
+
+
 
 }
--- src/main/java/com/appbuildersinc/attendance/source/restcontroller/Functions.java
+++ src/main/java/com/appbuildersinc/attendance/source/restcontroller/Functions.java
@@ -1,6 +1,7 @@
 package com.appbuildersinc.attendance.source.restcontroller;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +10,10 @@ public class Functions {
     public ResponseEntity<String> helloworld() {
         return ResponseEntity.ok("Hello, World!");
     }
+    @Bean
+    public ResponseEntity<String> byeworld() {
+        return ResponseEntity.ok("Bye, World!");
+    }
+
+
 }
