94716d19f72fccc863b1dc6d15975a79ab7de5a5
AvGeeky <sai.saip2005@gmail.com>
2025-05-21 23:01:33 +0530

    Initial commit

---
+++ src/main/java/com/appbuildersinc/attendance/SmartAttendanceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.appbuildersinc.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAttendanceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAttendanceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appbuildersinc/attendance/SmartAttendanceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appbuildersinc.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAttendanceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
