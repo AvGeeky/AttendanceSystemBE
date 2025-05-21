6b1db46937d81b359f5073d3a5d20623ef786e9b
AvGeeky <sai.saip2005@gmail.com>
2025-05-21 23:43:40 +0530

    test2

--- src/main/java/com/appbuildersinc/attendance/source/restcontroller/Controller.java
+++ src/main/java/com/appbuildersinc/attendance/source/restcontroller/Controller.java
@@ -1,12 +1,26 @@
 package com.appbuildersinc.attendance.source.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @GetMapping("/hi")
-    public String index() {
-        return "Hello World";
+    private final Functions functionsService;
+
+    @Autowired
+    public Controller(Functions functionsService) {
+        this.functionsService = functionsService;
     }
+    /**
+     * This endpoint returns a simple "Hello, World!" message.
+     *
+     * @return A ResponseEntity containing the "Hello, World!" message.
+     */
+    @GetMapping("/helloworld")
+    public ResponseEntity<String> setSecretId(){
+        return functionsService.helloworld();
+    }
+
 }
