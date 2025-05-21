bd10926c17aebc60cdc8e7ac5dfc31224bd16ed2
AvGeeky <sai.saip2005@gmail.com>
2025-05-22 00:16:59 +0530

    test4

--- src/main/java/com/appbuildersinc/attendance/source/restcontroller/Functions.java
+++ src/main/java/com/appbuildersinc/attendance/source/restcontroller/Functions.java
@@ -1,7 +1,6 @@
 package com.appbuildersinc.attendance.source.restcontroller;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +9,6 @@ public class Functions {
     public ResponseEntity<String> helloworld() {
         return ResponseEntity.ok("Hello, World!");
     }
-    @Bean
     public ResponseEntity<String> byeworld() {
         return ResponseEntity.ok("Bye, World!");
     }
