340b34432c8f4ce034319611b607b65ccfe3da55
AvGeeky <sai.saip2005@gmail.com>
2025-05-27 16:26:22 +0530

    adding secure redis docker compose setup v4 - ready

--- redis-setup/redis/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Inject the REDIS_PASSWORD from env into redis.conf
-envsubst < /usr/local/etc/redis/redis.conf.template > /usr/local/etc/redis/redis.conf
-
-exec redis-server /usr/local/etc/redis/redis.conf
