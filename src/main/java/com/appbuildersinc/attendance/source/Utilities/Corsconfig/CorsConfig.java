f7044a0b716adb9d269998f36457ebfc7e6b90df
Sai Pranav <saipranav.sai2005@gmail.com>
2025-07-03 23:15:33 +0530

    Add CORS configuration and update NGINX settings for improved security and request handling. Bump version to 0.3.4-BETA.

---
+++ src/main/java/com/appbuildersinc/attendance/source/Utilities/Corsconfig/CorsConfig.java
@@ -0,0 +1,14 @@
+package com.appbuildersinc.attendance.source.Utilities.Corsconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Disable Spring CORS entirely
+    }
+}
+
