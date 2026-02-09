baf9522eebb35418a7444bfd3047d6c81371dbf5
Sai Pranav <saipranav.sai2005@gmail.com>
2026-02-09 22:20:33 +0530

    Update Docker Compose and Nginx configuration; enhance Redis setup, add Prometheus and Grafana services, and improve connection pooling in RedisConfig

--- src/main/java/com/appbuildersinc/attendance/source/database/redis/RedisConfig.java
+++ src/main/java/com/appbuildersinc/attendance/source/database/redis/RedisConfig.java
@@ -1,12 +1,16 @@
 package com.appbuildersinc.attendance.source.database.redis;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.*;
-import org.springframework.data.redis.connection.lettuce.*;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -22,9 +26,26 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration cfg = new RedisStandaloneConfiguration(host, port);
-        cfg.setPassword(RedisPassword.of(password));
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder().build();
-        return new LettuceConnectionFactory(cfg, clientConfig);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+
+        // IMPROVEMENT 1: Connection Pooling
+        // This manages multiple connections so threads don't block each other
+        LettuceClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
+                .commandTimeout(Duration.ofMillis(4000))
+                .poolConfig(buildPoolConfig())
+                .build();
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig);
+    }
+
+    private org.apache.commons.pool2.impl.GenericObjectPoolConfig buildPoolConfig() {
+        org.apache.commons.pool2.impl.GenericObjectPoolConfig poolConfig = new org.apache.commons.pool2.impl.GenericObjectPoolConfig();
+        poolConfig.setMaxTotal(16); // Max active connections
+        poolConfig.setMaxIdle(8);   // Max idle connections waiting
+        poolConfig.setMinIdle(4);   // Min connections to keep alive
+        return poolConfig;
     }
 }
